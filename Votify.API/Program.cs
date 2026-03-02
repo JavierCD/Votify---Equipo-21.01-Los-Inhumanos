db7c3851ca0afd296b4ff18eff1037dd2100b1c3
JavierCD <javiercabreradeniz@gmail.com>
2026-03-02 23:28:35 +0100

    Agregar archivos de proyecto.

---
+++ Votify.API/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Votify.Persistence.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 1. Registrar el contexto de la base de datos (Persistencia)
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<VotifyContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// 2. Configurar CORS (Para que la Presentación pueda llamar a la REST API)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazor", policy =>
+        policy.AllowAnyOrigin() // En producción aquí pondrás la URL de tu web
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowBlazor"); // ¡Súper importante! Activa el permiso de comunicación
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Votify.Persistence/Context/VotifyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Votify.Persistence.Entities; // Asegúrate de que esta carpeta exista
+
+namespace Votify.Persistence.Context
+{
+    public class VotifyContext : DbContext
+    {
+        public VotifyContext(DbContextOptions<VotifyContext> options) : base(options)
+        {
+        }
+
+        // Aquí definiremos las tablas de tu base de datos
+        // public DbSet<Evento> Eventos { get; set; }
+    }
+}
