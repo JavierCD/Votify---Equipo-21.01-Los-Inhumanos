3c80c6fdecd2f64bbdcb37ff1a61676b92d7dd79
JavierCD <javiercabreradeniz@gmail.com>
2026-03-10 17:57:09 +0100

    Creadas la carpeta donde estarán las clases

---
+++ Votify.Persistence/Entities/Evento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Votify.Persistence.Entities
+{
+    internal class Evento
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del evento es obligatorio")]
+        [StringLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Descripcion { get; set; }
+
+        [Required]
+        public DateTime FechaInicio { get; set; }
+        [Required]
+        public DateTime FechaFin { get; set; }
+
+        //public ICollection<Participante> 
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
