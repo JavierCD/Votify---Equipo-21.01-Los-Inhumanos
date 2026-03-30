1efbb23766bea0dd4d3260f0c9d3ada8dc8c0a12
Daniel Arevalo Rodriguez <DAREROD@upv.edu.es>
2026-03-30 17:16:36 +0200

    ss

--- Votify.Core/Interfaces/IPopularRepository.cs
+++ Votify.Core/Interfaces/IPopularRepository.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Votify.Core.Models;
+﻿using Votify.Core.Models;
 
 namespace Votify.Core.Interfaces
 {
     public interface IPopularRepository
     {
-        Task<bool> EventoExisteAsync(int eventoId);
+        Task<bool> CategoriaExisteAsync(int categoriaId);
         Task<Popular> CrearAsync(Popular popular);
     }
 }
