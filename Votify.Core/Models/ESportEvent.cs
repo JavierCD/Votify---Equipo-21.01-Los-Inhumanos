0b4aa6836dd3f3a0a154892744df3f1658b0207e
JavierCD <javiercabreradeniz@gmail.com>
2026-04-05 17:45:05 +0100

    Implementación de Factory Method para clase Evento

---
+++ Votify.Core/Models/ESportEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Votify.Core.Models
+{
+    public class ESportsEvent : Evento
+    {
+        public ESportsEvent(string name, DateTime fechaInicio, DateTime fechaFin, int organizadorId, string? descripcion = null)
+            : base(name, fechaInicio, fechaFin, organizadorId, descripcion) { }
+
+        public override string Modalidad() => "E-Sports";
+    }
+}
