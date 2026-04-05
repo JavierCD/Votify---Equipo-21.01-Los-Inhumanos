3140ceaf7f1b63bee394f0c90c85d26dcbdec33e
Daniel Arevalo Rodriguez <DAREROD@upv.edu.es>
2026-04-05 17:03:25 +0200

    db initializer que ya habia en master

--- Votify.Persistence/Context/DBInitializer.cs
+++ Votify.Persistence/Context/DBInitializer.cs
@@ -12,12 +12,12 @@ namespace Votify.Persistence.Context
 {
     public static class DbInitializer
     {
+
         public static void Initialize(VotifyContext context)
         {
             context.Database.EnsureDeleted();
             context.Database.Migrate();
 
-            // 1. Miembros base
             var organizadorMock = new Organizador
             {
                 Name = "Alan Brito (Organizador Demo)",
@@ -39,38 +39,9 @@ namespace Votify.Persistence.Context
                 Password = "HashTemporal123"
             };
 
-            var participante2 = new Participante
-            {
-                Name = "Equipo Alpha",
-                Email = "alpha@votify.com",
-                Password = "HashTemporal123"
-            };
-
-            var participante3 = new Participante
-            {
-                Name = "Equipo Beta",
-                Email = "beta@votify.com",
-                Password = "HashTemporal123"
-            };
-
-            var participante4 = new Participante
-            {
-                Name = "Equipo Gamma",
-                Email = "gamma@votify.com",
-                Password = "HashTemporal123"
-            };
-
-            var participante5 = new Participante
-            {
-                Name = "Equipo Delta",
-                Email = "delta@votify.com",
-                Password = "HashTemporal123"
-            };
-
-            context.Miembros.AddRange(organizadorMock, juez, participante, participante2, participante3, participante4, participante5);
+            context.Miembros.AddRange(organizadorMock, juez, participante);
             context.SaveChanges();
 
-            // 2. Evento
             var eventoDemo = new Evento
             {
                 Name = "Hackathon de Innovación 2026",
@@ -85,7 +56,6 @@ namespace Votify.Persistence.Context
             context.Eventos.Add(eventoDemo);
             context.SaveChanges();
 
-            // 3. Categorías
             var categoria1 = new Categoria("Proyectos Sociales", "Soluciones tecnológicas con impacto social")
             {
                 Name = "Proyectos Sociales",
@@ -101,50 +71,6 @@ namespace Votify.Persistence.Context
             };
 
             context.Categorias.AddRange(categoria1, categoria2);
-            context.SaveChanges();
-
-            // 4. Proyectos asociados a categorías
-            var proyecto1 = new Proyecto
-            {
-                Name = "AgroTech Social",
-                Visible = true,
-                ParticipanteId = participante.Id,
-                Categorias = new List<Categoria> { categoria1 }
-            };
-
-            var proyecto2 = new Proyecto
-            {
-                Name = "EduAccess",
-                Visible = true,
-                ParticipanteId = participante2.Id,
-                Categorias = new List<Categoria> { categoria1 }
-            };
-
-            var proyecto3 = new Proyecto
-            {
-                Name = "HealthBot AI",
-                Visible = true,
-                ParticipanteId = participante3.Id,
-                Categorias = new List<Categoria> { categoria2 }
-            };
-
-            var proyecto4 = new Proyecto
-            {
-                Name = "SmartCity Assistant",
-                Visible = true,
-                ParticipanteId = participante4.Id,
-                Categorias = new List<Categoria> { categoria2 }
-            };
-
-            context.Proyectos.AddRange(proyecto1, proyecto2, proyecto3, proyecto4);
-            context.SaveChanges();
-
-            // 5. Votantes
-            var votante1 = new Votante { Email = "votante1@votify.com", Anonimo = false };
-            var votante2 = new Votante { Email = "votante2@votify.com", Anonimo = false };
-            var votante3 = new Votante { Email = "votante3@votify.com", Anonimo = true };
-
-            context.Votantes.AddRange(votante1, votante2, votante3);
             context.SaveChanges();
         }
     }
