f8264aefe6ef6961d83f72f27de3b633aabb87f4
JavierCD <javiercabreradeniz@gmail.com>
2026-04-28 17:08:29 +0200

    Comienzo del frontEnd para emitir un Voto de categoria Multicriterio

---
+++ Votify.Services/Models/EmitirVotoMulticriterioRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Votify.Services.Models
+{
+    public class EmitirVotoMulticriterioRequest
+    {
+        public int VotacionId { get; set; }
+        public string Email { get; set; } = string.Empty;
+
+        // Diccionario anidado: ProyectoId -> (CriterioId -> Puntuacion)
+        public Dictionary<int, Dictionary<int, int>> Puntuaciones { get; set; } = new();
+    }
+}
---
+++ Votify.Services/Models/VotacionMulticriterioDetalleResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Votify.Services.Models
+{
+    public class VotacionMulticriterioDetalleResponse
+    {
+        public int VotacionId { get; set; }
+        public string CategoriaNombre { get; set; } = string.Empty;
+        public string Estado { get; set; } = string.Empty;
+        public List<ProyectoDto> Proyectos { get; set; } = new();
+        public List<CriterioBaremoDto> Criterios { get; set; } = new();
+
+        public class ProyectoDto
+        {
+            public int Id { get; set; }
+            public string Name { get; set; } = string.Empty;
+        }
+
+        public class CriterioBaremoDto
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; } = string.Empty;
+            public int Peso { get; set; }
+        }
+    }
+}
