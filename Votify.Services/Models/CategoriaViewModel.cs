0249c0be585f848dab88163da8605c4d6ac10088
JavierCD <javiercabreradeniz@gmail.com>
2026-04-29 15:55:09 +0200

    Cambios para cumplir por completo la Clean Architecture

---
+++ Votify.Services/Models/CategoriaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Votify.Services.Models
+{
+    public class CategoriaViewModel
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+    }
+}
