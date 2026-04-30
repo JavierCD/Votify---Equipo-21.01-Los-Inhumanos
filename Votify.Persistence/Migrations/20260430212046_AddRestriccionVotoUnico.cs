40aaa883fd57e87b43c68e165c04b4aa60a1cd45
Daniel Arevalo Rodriguez <DAREROD@upv.edu.es>
2026-04-30 23:25:15 +0200

    UT restricciones finalizada

---
+++ Votify.Persistence/Migrations/20260430212046_AddRestriccionVotoUnico.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Votify.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRestriccionVotoUnico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "PermiteAutoVoto",
+                table: "Votaciones",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RestriccionVotoUnico",
+                table: "Votaciones",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RestriccionVotoUnico",
+                table: "Votaciones");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "PermiteAutoVoto",
+                table: "Votaciones",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+    }
+}
